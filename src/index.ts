49642b5ab55741315872b3cd239212d211d58ef1
Jonas Lüthke <joluet@users.noreply.github.com>
2023-02-28 18:49:58 +0100

    Add function getLocalizedCurrencyName (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,11 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+import ReactNativeCurrencyLocalizationModule from "./ReactNativeCurrencyLocalizationModule";
 
-// Import the native module. On web, it will be resolved to ReactNativeCurrencyLocalization.web.ts
-// and on native platforms to ReactNativeCurrencyLocalization.ts
-import ReactNativeCurrencyLocalizationModule from './ReactNativeCurrencyLocalizationModule';
-import ReactNativeCurrencyLocalizationView from './ReactNativeCurrencyLocalizationView';
-import { ChangeEventPayload, ReactNativeCurrencyLocalizationViewProps } from './ReactNativeCurrencyLocalization.types';
-
-// Get the native constant value.
-export const PI = ReactNativeCurrencyLocalizationModule.PI;
-
-export function hello(): string {
-  return ReactNativeCurrencyLocalizationModule.hello();
+export function getLocalizedCurrencyName(
+  locale: string,
+  currencyCode: string
+): string {
+  return ReactNativeCurrencyLocalizationModule.getLocalizedCurrencyName(
+    locale,
+    currencyCode
+  );
 }
-
-export async function setValueAsync(value: string) {
-  return await ReactNativeCurrencyLocalizationModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ReactNativeCurrencyLocalizationModule ?? NativeModulesProxy.ReactNativeCurrencyLocalization);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ReactNativeCurrencyLocalizationView, ReactNativeCurrencyLocalizationViewProps, ChangeEventPayload };
