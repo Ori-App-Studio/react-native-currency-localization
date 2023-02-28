0dc41ef1695bb4afa3a424766bcca9c2691a0d7d
jonas <jonas@luethke.net>
2023-02-28 17:11:37 +0100

    Add function getLocalizedCurrencyName

--- example/App.tsx
+++ example/App.tsx
@@ -1,20 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import * as ReactNativeCurrencyLocalization from 'react-native-currency-localization';
+import { Text, View } from "react-native";
+import * as ReactNativeCurrencyLocalization from "react-native-currency-localization";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>{ReactNativeCurrencyLocalization.hello()}</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Currency: 'USD', locale: 'en-CA'</Text>
+      <Text>
+        {ReactNativeCurrencyLocalization.getLocalizedCurrencyName(
+          "en-CA",
+          "USD"
+        )}
+      </Text>
+      <Text>Currency: 'USD', locale: 'ar'</Text>
+      <Text>
+        {ReactNativeCurrencyLocalization.getLocalizedCurrencyName("ar", "USD")}
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
