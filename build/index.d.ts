6b075583907f3ccc15df5c3706dc1c68dfc2c6ba
jonas <jonas@luethke.net>
2023-03-01 17:09:24 +0100

    Add build folder to repo

---
+++ build/index.d.ts
@@ -0,0 +1,2 @@
+export declare function getLocalizedCurrencyName(locale: string, currencyCode: string): string;
+//# sourceMappingURL=index.d.ts.map
---
+++ build/index.js
@@ -0,0 +1,5 @@
+import ReactNativeCurrencyLocalizationModule from "./ReactNativeCurrencyLocalizationModule";
+export function getLocalizedCurrencyName(locale, currencyCode) {
+    return ReactNativeCurrencyLocalizationModule.getLocalizedCurrencyName(locale, currencyCode);
+}
+//# sourceMappingURL=index.js.map
